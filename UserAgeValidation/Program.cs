d83429e327fedd20035443f1c5b000a443f5c480
Karl Kryynvald <karl.gustav.krnvald@tktk.ee>
2024-02-01 20:53:09 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Rakendus küsib kasutajalt valida tema sugu (m/f)
+// Rakendus küsib kasutajalt tema perekonna nime
+// Lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+// "Welcome , Mr. [kasutaja perekonnanimi]" / "Welcome , Ms. [kasutaja perekonnanimi]"
+
+using System.Threading.Channels;
+
+Console.WriteLine("Please select Your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your Last name:");
+String userLastName = Console.ReadLine();
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
+
+
+
+
+
+
+
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Rakendus küsib kasutajalt tema vanust
+// Kui kasutaja vanus on väiksem, kui 13, siis konsoolist kuvatakse:
+// "You are too young, come back when older"
+// muul juhul
+// Konsoolis kuvatakse "Welcome to Instagram"
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if (isAgeNumber == true) 
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young, come back when older.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read Your age!");
+}
+
+
+
+
+
+
+//int userAge = Int32.Parse(Console.ReadLine());//"13" - heap, 13 - stack
+
+//if (userAge >= 13)
+//{
+//    Console.WriteLine("Welcome to Instagram!");
+//}
+//else
+//{
+//    Console.WriteLine("You are too young, come back when older.");
+//}
+
+
+
+
+
+
+
+
